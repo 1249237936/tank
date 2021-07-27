7e865d6b48dddebd7c14914101dc360ef3fbf493
Jason <15521215849@163.com>
2021-07-27 17:51:20 +0800

    去掉初始化部分，为网络版做准备

--- src/main/java/com/jason/tank/Main.java
+++ src/main/java/com/jason/tank/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf = new TankFrame();
 
-        int initTankCount = PropertyMgr.getInt("initTankCount");
+        /*int initTankCount = PropertyMgr.getInt("initTankCount");
 
         for (int i = 0; i < initTankCount; i++) {
             tf.tanks.add(new Tank(50 + i * 80, 200, Dir.DOWN, Group.BAD, tf));
-        }
+        }*/
 
         new Thread(() -> new Audio("audio/war1.wav").loop()).start();
 
